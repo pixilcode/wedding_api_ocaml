612df7474429c20476dd010408161f3e88a89964
Brendon Bown <brendon.bown@gmail.com>
2024-06-30 23:29:40 -0600

    Add some basic auth

--- bin/main.ml
+++ bin/main.ml
@@ -8,17 +8,27 @@ let concat_fields form_fields: string =
   |> List.map ~f:(fun (key, value) -> Printf.sprintf "%s=%s\n" key value)
   |> String.concat
 
+let decode_basic_auth auth_header =
+  match String.split ~on:' ' auth_header with
+  | "Basic" :: encoded :: _ -> (
+    Some (Base64.decode_exn encoded)
+  )
+  | _ -> None
+
+
 let () =
   Dream.run
   @@ Dream.logger
   @@ Dream.router [
     Dream.get "/file" (fun _ ->
       let contents = File.read_file test_path in
-      Dream.html contents);
+      Dream.html contents
+    );
     
     Dream.post "/createFile" (fun _ ->
       File.create_file test_path;
-      Dream.html "file created!\n");
+      Dream.html "file created!\n"
+    );
 
     Dream.put "/updateContents" (fun request ->
       let%lwt body = Dream.form ~csrf:false request in
@@ -28,5 +38,23 @@ let () =
         File.write_to_file test_path file_contents;
         Dream.html "Updated!\n"
       | _ -> Dream.empty `Bad_Request
+    );
+
+    Dream.get "/authenticated" (fun request ->
+      let auth_header = Dream.header request "Authorization" in
+      match auth_header with
+      | Some auth_header ->
+        let decoded = decode_basic_auth auth_header in
+        let body = match decoded with
+          | Some decoded -> Printf.sprintf "Header: '%s'\nDecoded value: '%s'" auth_header decoded
+          | None -> "Invalid Authorization header"
+        in
+        Dream.html body
+      | None -> (
+        Dream.respond
+          ~status:`Unauthorized
+          ~headers:["WWW-Authenticate", "Basic realm=\"Dream\""]
+          "Unauthorized"
+      )
     )
    ]
